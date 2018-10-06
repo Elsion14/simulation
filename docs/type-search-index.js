5c9ae4861341693d9d6b11e3b011a4679932f4bc
Elsion14 <21606807@unicaen.fr>
2018-10-06 17:28:19 +0200

    javadoc méthode addNation classe World

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"p":"game","l":"Building"},{"p":"game","l":"City"},{"p":"game","l":"CityTiles"},{"p":"game","l":"DemographicResource"},{"p":"game","l":"Main"},{"p":"game","l":"Nation"},{"p":"game","l":"Plain"},{"p":"game","l":"RicherResource"},{"p":"game","l":"Tile"},{"p":"game","l":"World"}]
+typeSearchIndex = [{"p":"game","l":"Building"},{"p":"game","l":"City"},{"p":"game","l":"DemographicResource"},{"p":"game","l":"Main"},{"p":"game","l":"Nation"},{"p":"game","l":"Plain"},{"p":"game","l":"RicherResource"},{"p":"game","l":"Tile"},{"p":"game","l":"World"}]
